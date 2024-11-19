2dabf3af28c79044e9ba4cf31b75092ff2bf37ef
anitapatil2814 <anitapatil2814@gmail.com>
2024-11-19 20:51:58 +0530

    first commit

---
+++ Placement_Portal/src/main/java/com/demo/repository/PlacementDriveRepository.java
@@ -0,0 +1,9 @@
+package com.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demo.entity.PlacementDrive;
+
+public interface PlacementDriveRepository extends JpaRepository<PlacementDrive, Long> {
+
+}
